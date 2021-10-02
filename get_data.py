80428c891c0b383de0b45754b9d618ec81441d05
jh-chee <hardfastfurioushff@gmail.com>
2021-10-02 21:54:45 +0800

    refactor loading functions for readibilty

--- get_data.py
+++ get_data.py
@@ -37,84 +37,169 @@ class Getdata(torch.utils.data.Dataset):
         self.len_watermark = len(self.watermark_files)
         self.len_wallpaper = len(self.wallpaper_files)
 
+
     def __getitem__(self, index):
         img_id = self.photo_files[index]
         img_J, img_I, mask, balance, alpha, w = self.generate_mask(img_id)
         return img_J, img_I, mask, balance, alpha, w
 
+
     def __len__(self):
         return len(self.photo_files)
+
+
+    def load_watermark(self, img, alpha, W):
+        """
+        Loads a random watermark on original image
+        
+        Parameters
+        ----------
+        img: torch.Tensor
+            Original image without watermarks
+        alpha: torch.Tensor
+            Alpha transparency, from 0 to 1
+        W: torch.Tensor
+            Blank canvas for holding watermarks
+        
+        Returns
+        -------
+        img: torch.Tensor
+            Watermarked image
+        W: torch.Tensor
+            Canvas after watermark operation
+        """
+
+        logo_id = self.watermark_files[torch.randint(1, self.len_watermark, (1,)).item()]
+        logo = Image.open(osp.join(self.watermark_path, logo_id)).convert('RGBA')
+
+        rotate_angle = torch.randint(0, 360, (1,))
+        logo_height = torch.randint(10, self.img_height, (1,))
+        logo_width = torch.randint(10, self.img_width, (1,))
+
+        logo_rotate = logo.rotate(rotate_angle, expand=True)
+        logo_resize = logo_rotate.resize((logo_height, logo_width))
+        logo = self.transform_logo_to_tensor(logo_resize)
+
+        start_height = torch.randint(0, self.img_height - logo_height.item(), (1,))
+        start_width = torch.randint(0, self.img_width - logo_width.item(), (1,))
+
+        img[:, start_width:start_width + logo_width, start_height:start_height + logo_height] *= \
+            (1.0 - alpha * logo[3:4, :, :]) + logo[:3, :, :] * alpha * logo[3:4, :, :]
+
+        W[:, start_width:start_width + logo_width, start_height:start_height + logo_height] += logo[:3, :, :]
+
+        return img, W
+
+
+    def load_wallpaper(self, img, alpha, W):
+        """
+        Loads a random wallpaper on original image
+        
+        Parameters
+        ----------
+        img: torch.Tensor
+            Watermarked image
+        alpha: torch.Tensor
+            Alpha transparency, from 0 to 1
+        W: torch.Tensor
+            Watermarked canvas
+        
+        Returns
+        -------
+        img: torch.Tensor
+            Watermarked and wallpapered image
+        W: torch.Tensor
+            Canvas after watermark and wallpaper operation
+        """
+
+        wallpaper_id = self.wallpaper_files[torch.randint(1, self.len_wallpaper, (1,)).item()]
+        wallpaper = Image.open(osp.join(self.wallpaper_path, wallpaper_id)).convert('RGBA')
+
+        wallpaper_resize = wallpaper.resize((self.img_height, self.img_width))
+        wallpaper = self.transform_logo_to_tensor(wallpaper_resize)
+
+        img *= (1.0 - alpha * wallpaper[3:4, :, :]) + wallpaper[:3, :, :] * alpha * wallpaper[3:4, :, :]
+
+        W += wallpaper[:3, :, :]  # some values will be > 1
+        W = W / torch.max(W)  # normalise the values to be between 0 and 1
+
+        return img, W
+
+
+    def load_words(self, img, W):
+        """
+        Loads a random wallpaper on original image
+        
+        Parameters
+        ----------
+        img: torch.Tensor
+            Watermarked and wallpapered image
+        alpha: torch.Tensor
+            Alpha transparency, from 0 to 1
+        W: torch.Tensor
+            Watermarked and wallpapered canvas
+        
+        Returns
+        -------
+        img: torch.Tensor
+            Watermarked and wallpapered and worded image
+        W: torch.Tensor
+            Canvas after watermark and wallpaper and wording operation
+        """
+
+        img_copy = Image.fromarray(np.array(img.permute(1, 2, 0) * 255).astype(np.uint8))
+        # _, width, height = np.array(img).shape
+        canvas = Image.new('RGB', (self.img_width, self.img_height), (0, 0, 0))
+        d = ImageDraw.Draw(img_copy)
+        d_canvas = ImageDraw.Draw(canvas)
+
+        num_words = torch.randint(1, 10, (1,)).item()
+        string_list = string.digits + string.ascii_letters
+
+        for _ in range(num_words):
+            rand_width = torch.rand(1) * self.img_width
+            rand_height = torch.rand(1) * self.img_height
+            rand_string = ''.join([string_list[torch.randint(0, len(string_list), (1,)).item()] for x in range(20)])
+            rand_fill = (torch.randint(0, 255, (1,)), torch.randint(0, 255, (1,)), torch.randint(0, 255, (1,)))
+
+            d.text((rand_width, rand_height), rand_string, fill=rand_fill)
+            d_canvas.text((rand_width, rand_height), rand_string, fill=rand_fill)
+
+        img = (torch.from_numpy(np.array(img_copy)).permute(2, 0, 1) / 255).to(torch.float32)
+        text_img = (torch.from_numpy(np.array(canvas)).permute(2, 0, 1) / 255).to(torch.float32)
+        W += text_img[:3, :, :]
+        W = W / torch.max(W)
+
+        return img, W
+
 
     def generate_mask(self, img_id):
         """
         Generates watermarked_image, Watermark_free_image, Mask, Loss_balance, Alpha, Watermark
-        based on input photo, with a probability of 0.8 to load a random watermark
+        based on input photo
         """
 
         img = Image.open(osp.join(self.photo_path, img_id))
         img = self.transform_img_to_tensor(img)
-        img_height, img_width = img.shape[2], img.shape[1]  # 200, 200
+        self.img_height, self.img_width = img.shape[2], img.shape[1]  # 200, 200
         img_I = img.clone()  # watermark free
 
         alpha = torch.rand(1) * 0.4 + 0.5
         W = torch.zeros_like(img)
 
-        load_watermark = torch.rand(1) < 0.3
-        if load_watermark:
-            logo_id = self.watermark_files[torch.randint(1, self.len_watermark, (1,)).item()]
-            logo = Image.open(osp.join(self.watermark_path, logo_id)).convert('RGBA')
-            rotate_angle = torch.randint(0, 360, (1,))
-            logo_height = torch.randint(10, img_height, (1,))
-            logo_width = torch.randint(10, img_width, (1,))
-            logo_rotate = logo.rotate(rotate_angle, expand=True)
-            logo_resize = logo_rotate.resize((logo_height, logo_width))
-            logo = self.transform_logo_to_tensor(logo_resize)
+        # load_watermark
+        if torch.rand(1) < 0.3:
+            img, W = self.load_watermark(img, alpha, W)
 
-            start_height = torch.randint(0, img_height - logo_height.item(), (1,))
-            start_width = torch.randint(0, img_width - logo_width.item(), (1,))
+        # load_wallpaper
+        if torch.rand(1) < 0.0:  # disable for now
+            img, W = self.load_wallpaper(img, alpha, W)
 
-            img[:, start_width:start_width + logo_width, start_height:start_height + logo_height] *= \
-                (1.0 - alpha * logo[3:4, :, :]) + logo[:3, :, :] * alpha * logo[3:4, :, :]
+        # load_words
+        if torch.rand(1) < 0.3:
+            img, W = self.load_words(img, W)
 
-            W[:, start_width:start_width + logo_width, start_height:start_height + logo_height] += logo[:3, :, :]
-
-        load_wallpaper = torch.rand(1) < 0.0  # disable for now
-        if load_wallpaper:
-            wallpaper_id = self.wallpaper_files[torch.randint(1, self.len_wallpaper, (1,)).item()]
-            wallpaper = Image.open(osp.join(self.wallpaper_path, wallpaper_id)).convert('RGBA')
-
-            wallpaper_resize = wallpaper.resize((img_height, img_width))
-            wallpaper = self.transform_logo_to_tensor(wallpaper_resize)
-
-            img *= (1.0 - alpha * wallpaper[3:4, :, :]) + wallpaper[:3, :, :] * alpha * wallpaper[3:4, :, :]
-
-            W += wallpaper[:3, :, :]  # some values will be > 1
-            W = W / torch.max(W)  # normalise the values to be between 0 and 1
-
-        load_words = torch.rand(1) < 0.3
-        if load_words:
-            img_copy = Image.fromarray(np.array(img.permute(1, 2, 0) * 255).astype(np.uint8))
-            _, width, height = np.array(img).shape
-            canvas = Image.new('RGB', (width, height), (0, 0, 0))
-            d = ImageDraw.Draw(img_copy)
-            d_canvas = ImageDraw.Draw(canvas)
-
-            num_words = torch.randint(1, 10, (1,)).item()
-            string_list = string.digits + string.ascii_letters
-            for i in range(num_words):
-                rand_width = torch.rand(1) * width
-                rand_height = torch.rand(1) * height
-                rand_string = ''.join([string_list[torch.randint(0, len(string_list), (1,)).item()] for x in range(20)])
-                rand_fill = (torch.randint(0, 255, (1,)), torch.randint(0, 255, (1,)), torch.randint(0, 255, (1,)))
-                d.text((rand_width, rand_height), rand_string, fill=rand_fill)
-                d_canvas.text((rand_width, rand_height), rand_string, fill=rand_fill)
-
-            img = (torch.from_numpy(np.array(img_copy)).permute(2, 0, 1) / 255).to(torch.float32)
-            text_img = (torch.from_numpy(np.array(canvas)).permute(2, 0, 1) / 255).to(torch.float32)
-            W += text_img[:3, :, :]
-            W = W / torch.max(W)
-
-        img_J = img  # with watermark
+        img_J = img  
 
         mask = self.solve_mask(img_J, img_I)
         mask_saved = torch.cat((mask[:, :, None], mask[:, :, None], mask[:, :, None]), 2)
@@ -130,6 +215,7 @@ class Getdata(torch.utils.data.Dataset):
         alpha = alpha.permute(2, 0, 1)
         return img_J, img_I, mask_saved, balance_saved, alpha, W
 
+
     @staticmethod
     def solve_mask(img, img_target):
         img3 = torch.abs(img - img_target)
@@ -137,8 +223,9 @@ class Getdata(torch.utils.data.Dataset):
         mask = mask.to(torch.float32)
         return mask
 
+
     @staticmethod
-    def solve_balance( mask):
+    def solve_balance(mask):
         height, width = mask.shape
         k = int(mask.sum())
 
